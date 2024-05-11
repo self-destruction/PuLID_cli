01c492bfc9d2f951a55f4e511d8ede3f8aee9da2
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-11 21:16:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import spaces
-import gradio as gr
 import numpy as np
 import torch
+import argparse
+from PIL import Image
 
 from pulid import attention_processor as attention
 from pulid.pipeline import PuLIDPipeline
@@ -11,20 +12,81 @@ torch.set_grad_enabled(False)
 
 pipeline = PuLIDPipeline()
 
-# other params
 DEFAULT_NEGATIVE_PROMPT = (
     'flaws in the eyes, flaws in the face, flaws, lowres, non-HDRi, low quality, worst quality,'
     'artifacts noise, text, watermark, glitch, deformed, mutated, ugly, disfigured, hands, '
-    'low resolution, partially rendered objects,  deformed or partially rendered eyes, '
-    'deformed, deformed eyeballs, cross-eyed,blurry'
+    'low resolution, partially rendered objects, deformed or partially rendered eyes, '
+    'deformed, deformed eyeballs, cross-eyed, blurry'
 )
+
+# # realistic
+# --prompt 'portrait, cinematic, wolf ears, white hair' --mode 'fidelity'
+# # painting style
+# --prompt 'portrait, impressionist painting, loose brushwork, vibrant color, light and shadow play' --mode 'fidelity'
+# # papercut style
+# --prompt 'portrait, flat papercut style, silhouette, clean cuts, paper, sharp edges, minimalist, color block, man' --mode 'fidelity'
+# # 3d style
+# --prompt 'woman, cartoon, solo, Popmart Blind Box, Super Mario, 3d' --mode 'fidelity'
+# # anime style
+# --prompt 'portrait, the legend of zelda, anime' --mode 'fidelity'
+# # id mix
+# --prompt 'portrait, superman' --mode 'fidelity' --id_mix
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--save_dir", type=str)
+parser.add_argument("--save_file_name", type=str)
+# ID image (main)
+parser.add_argument("--face_img", type=str, default='')
+# Additional ID image (auxiliary)
+parser.add_argument("--supp_face_img_1", type=str, default='')
+# Additional ID image (auxiliary)
+parser.add_argument("--supp_face_img_2", type=str, default='')
+# Additional ID image (auxiliary)
+parser.add_argument("--supp_face_img_3", type=str, default='')
+# Prompt
+parser.add_argument("--prompt", type=str, default='portrait, cinematic, wolf ears, white hair')
+# Negative Prompt
+parser.add_argument("--neg_prompt", type=str, default=DEFAULT_NEGATIVE_PROMPT)
+# CFG, recommend value range [1, 1.5], 1 will be faster
+parser.add_argument("--cfg_scale", type=float, default=1.2)
+# Num samples [1, 8]
+parser.add_argument("--n_samples", type=int, default=1)
+parser.add_argument("--seed", type=int, default=-1)
+parser.add_argument("--steps", type=int, default=20)
+# Height
+parser.add_argument("--h", type=int, default=1024)
+# Width
+parser.add_argument("--w", type=int, default=768)
+# ID scale [0.00, 5.00]
+parser.add_argument("--id_scale", type=float, default=0.80)
+# ['fidelity', 'extremely style']
+parser.add_argument("--mode", type=str, default='fidelity', choices=['fidelity', 'extremely style'])
+# ID Mix (if you want to mix two ID image, please turn this on, otherwise, turn this off)
+parser.add_argument("--id_mix", action='store_true', default=False)
+args = parser.parse_args()
+print(args)
 
 
 @spaces.GPU
-def run(*args):
-    id_image = args[0]
-    supp_images = args[1:4]
-    prompt, neg_prompt, scale, n_samples, seed, steps, H, W, id_scale, mode, id_mix = args[4:]
+def run(
+    face_image,
+    supp_image1,
+    supp_image2,
+    supp_image3,
+    prompt,
+    neg_prompt,
+    scale,
+    n_samples,
+    seed,
+    steps,
+    H,
+    W,
+    id_scale,
+    mode,
+    id_mix
+):
+    id_image = Image.open(face_image).convert("RGB")
+    supp_images = [Image.open(name).convert("RGB") for name in [supp_image1, supp_image2, supp_image3] if name != '']
 
     pipeline.debug_img_list = []
     if mode == 'fidelity':
@@ -42,12 +104,11 @@ def run(*args):
         id_image = resize_numpy_image_long(id_image, 1024)
         id_embeddings = pipeline.get_id_embedding(id_image)
         for supp_id_image in supp_images:
-            if supp_id_image is not None:
-                supp_id_image = resize_numpy_image_long(supp_id_image, 1024)
-                supp_id_embeddings = pipeline.get_id_embedding(supp_id_image)
-                id_embeddings = torch.cat(
-                    (id_embeddings, supp_id_embeddings if id_mix else supp_id_embeddings[:, :5]), dim=1
-                )
+            supp_id_image = resize_numpy_image_long(Image.open(supp_id_image).convert("RGB"), 1024)
+            supp_id_embeddings = pipeline.get_id_embedding(supp_id_image)
+            id_embeddings = torch.cat(
+                (id_embeddings, supp_id_embeddings if id_mix else supp_id_embeddings[:, :5]), dim=1
+            )
     else:
         id_embeddings = None
 
@@ -57,169 +118,26 @@ def run(*args):
         img = pipeline.inference(prompt, (1, H, W), neg_prompt, id_embeddings, id_scale, scale, steps)[0]
         ims.append(np.array(img))
 
-    return ims, pipeline.debug_img_list
+    return ims
 
 
-_HEADER_ = '''
-<h2><b>Official 🤗 Gradio Demo</b></h2><h2><a href='https://github.com/ToTheBeginning/PuLID' target='_blank'><b>PuLID: Pure and Lightning ID Customization via Contrastive Alignment</b></a></h2>
+ims = run(
+    args.face_img,
+    args.supp_face_img_1,
+    args.supp_face_img_2,
+    args.supp_face_img_3,
+    args.prompt,
+    args.neg_prompt,
+    args.cfg_scale,
+    args.n_samples,
+    args.seed,
+    args.steps,
+    args.h,
+    args.w,
+    args.id_scale,
+    args.mode,
+    args.id_mix
+)
 
-**PuLID** is a tuning-free ID customization approach. PuLID maintains high ID fidelity while effectively reducing interference with the original model’s behavior.
-
-Code: <a href='https://github.com/ToTheBeginning/PuLID' target='_blank'>GitHub</a>. Techenical report: <a href='https://arxiv.org/abs/2404.16022' target='_blank'>ArXiv</a>.
-
-❗️❗️❗️**Tips:**
-- we provide some examples in the bottom, you can try these example prompts first
-- a single ID image is usually sufficient, you can also supplement with additional auxiliary images
-- We offer two modes: fidelity mode and extremely style mode. In most cases, the default fidelity mode should suffice. If you find that the generated results are not stylized enough, you can choose the extremely style mode.
-
-'''  # noqa E501
-
-_CITE_ = r"""
-If PuLID is helpful, please help to ⭐ the <a href='https://github.com/ToTheBeginning/PuLID' target='_blank'>Github Repo</a>. Thanks! [![GitHub Stars](https://img.shields.io/github/stars/ToTheBeginning/PuLID?style=social)](https://github.com/ToTheBeginning/PuLID)
----
-🚀 **Share**
-If you have generated satisfying or interesting images with PuLID, please share them with us or your friends!
-
-📝 **Citation**
-If you find our work useful for your research or applications, please cite using this bibtex:
-```bibtex
-@article{guo2024pulid,
-  title={PuLID: Pure and Lightning ID Customization via Contrastive Alignment},
-  author={Guo, Zinan and Wu, Yanze and Chen, Zhuowei and Chen, Lang and He, Qian},
-  journal={arXiv preprint arXiv:2404.16022},
-  year={2024}
-}
-```
-
-📋 **License**
-Apache-2.0 LICENSE. Please refer to the [LICENSE file](placeholder) for details.
-
-📧 **Contact**
-If you have any questions, feel free to open a discussion or contact us at <b>wuyanze123@gmail.com</b> or <b>guozinan.1@bytedance.com</b>.
-"""  # noqa E501
-
-
-with gr.Blocks(title="PuLID", css=".gr-box {border-color: #8136e2}") as demo:
-    gr.Markdown(_HEADER_)
-    with gr.Row():
-        with gr.Column():
-            with gr.Row():
-                face_image = gr.Image(label="ID image (main)", sources="upload", type="numpy", height=256)
-                supp_image1 = gr.Image(
-                    label="Additional ID image (auxiliary)", sources="upload", type="numpy", height=256
-                )
-                supp_image2 = gr.Image(
-                    label="Additional ID image (auxiliary)", sources="upload", type="numpy", height=256
-                )
-                supp_image3 = gr.Image(
-                    label="Additional ID image (auxiliary)", sources="upload", type="numpy", height=256
-                )
-            prompt = gr.Textbox(label="Prompt", value='portrait,cinematic,wolf ears,white hair')
-            submit = gr.Button("Generate")
-            neg_prompt = gr.Textbox(label="Negative Prompt", value=DEFAULT_NEGATIVE_PROMPT)
-            scale = gr.Slider(
-                label="CFG, recommend value range [1, 1.5], 1 will be faster ",
-                value=1.2,
-                minimum=1,
-                maximum=1.5,
-                step=0.1,
-            )
-            n_samples = gr.Slider(label="Num samples", value=4, minimum=1, maximum=4, step=1)
-            seed = gr.Slider(
-                label="Seed", value=42, minimum=np.iinfo(np.uint32).min, maximum=np.iinfo(np.uint32).max, step=1
-            )
-            steps = gr.Slider(label="Steps", value=4, minimum=1, maximum=100, step=1)
-            with gr.Row():
-                H = gr.Slider(label="Height", value=1024, minimum=512, maximum=1280, step=64)
-                W = gr.Slider(label="Width", value=768, minimum=512, maximum=1280, step=64)
-            with gr.Row():
-                id_scale = gr.Slider(label="ID scale", minimum=0, maximum=5, step=0.05, value=0.8, interactive=True)
-                mode = gr.Dropdown(label="mode", choices=['fidelity', 'extremely style'], value='fidelity')
-                id_mix = gr.Checkbox(
-                    label="ID Mix (if you want to mix two ID image, please turn this on, otherwise, turn this off)",
-                    value=False,
-                )
-
-            gr.Markdown("## Examples")
-            example_inps = [
-                [
-                    'portrait,cinematic,wolf ears,white hair',
-                    'example_inputs/liuyifei.png',
-                    'fidelity',
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, mode], label='realistic')
-
-            example_inps = [
-                [
-                    'portrait, impressionist painting, loose brushwork, vibrant color, light and shadow play',
-                    'example_inputs/zcy.webp',
-                    'fidelity',
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, mode], label='painting style')
-
-            example_inps = [
-                [
-                    'portrait, flat papercut style, silhouette, clean cuts, paper, sharp edges, minimalist,color block,man',
-                    'example_inputs/lecun.jpg',
-                    'fidelity',
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, mode], label='papercut style')
-
-            example_inps = [
-                [
-                    'woman,cartoon,solo,Popmart Blind Box, Super Mario, 3d',
-                    'example_inputs/rihanna.webp',
-                    'fidelity',
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, mode], label='3d style')
-
-            example_inps = [
-                [
-                    'portrait, the legend of zelda, anime',
-                    'example_inputs/liuyifei.png',
-                    'extremely style',
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, mode], label='anime style')
-
-            example_inps = [
-                [
-                    'portrait, superman',
-                    'example_inputs/lecun.jpg',
-                    'example_inputs/lifeifei.jpg',
-                    'fidelity',
-                    True,
-                ]
-            ]
-            gr.Examples(examples=example_inps, inputs=[prompt, face_image, supp_image1, mode, id_mix], label='id mix')
-
-        with gr.Column():
-            output = gr.Gallery(label='Output', elem_id="gallery")
-            intermediate_output = gr.Gallery(label='DebugImage', elem_id="gallery", visible=False)
-            gr.Markdown(_CITE_)
-
-    inps = [
-        face_image,
-        supp_image1,
-        supp_image2,
-        supp_image3,
-        prompt,
-        neg_prompt,
-        scale,
-        n_samples,
-        seed,
-        steps,
-        H,
-        W,
-        id_scale,
-        mode,
-        id_mix,
-    ]
-    submit.click(fn=run, inputs=inps, outputs=[output, intermediate_output])
-
-
-demo.launch()
+for i, img in enumerate(ims):
+    Image.fromarray(x_sample).save(f'{args.save_dir}/{args.save_file_name}_{i}.png')
