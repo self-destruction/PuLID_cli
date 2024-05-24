771e0f236391395ea32ef0533c8768e0a36af56d
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-24 19:23:07 +0500

    Update pipeline.py

--- pulid/pipeline.py
+++ pulid/pipeline.py
@@ -35,8 +35,8 @@ class PuLIDPipeline:
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.device = 'cuda'
-        # sdxl_base_repo = 'RunDiffusion/Juggernaut-XL-v9'
-        sdxl_base_repo = 'SG161222/RealVisXL_V4.0'
+        sdxl_base_repo = 'RunDiffusion/Juggernaut-XL-v9'
+        # sdxl_base_repo = 'SG161222/RealVisXL_V4.0'
         self.sdxl_base_repo = sdxl_base_repo
 
         self.pipe = StableDiffusionXLPipeline.from_pretrained(
