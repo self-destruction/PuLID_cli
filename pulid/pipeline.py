db944dc47ac74d1ff595660294dcb80ad8980261
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-25 19:17:22 +0500

    Update pipeline.py

--- pulid/pipeline.py
+++ pulid/pipeline.py
@@ -37,7 +37,7 @@ class PuLIDPipeline:
         self.device = 'cuda'
         # sdxl_base_repo = 'RunDiffusion/Juggernaut-XL-v9'
         # sdxl_base_repo = 'SG161222/RealVisXL_V4.0'
-        sdxl_base_repo = 'Magamanny/Pony-Diffusion-V6-XL'
+        sdxl_base_repo = 'fancyfeast/pony-diffusion-v6-xl-diffusers'
         self.sdxl_base_repo = sdxl_base_repo
 
         self.pipe = StableDiffusionXLPipeline.from_pretrained(
